61f778e204c9d137bf1d2b48705f51839206ae78
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-27 19:49:01 +0800

    master SpringMVC配置文件

---
+++ spring_mvc_helloworld/src/main/java/com/bylearning/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.bylearning.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/9/27 19:33
+ * @description TODO
+ */
+@Controller
+public class HelloController {
+
+
+}
