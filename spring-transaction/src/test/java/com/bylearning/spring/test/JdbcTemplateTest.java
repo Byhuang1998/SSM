b1712cc76b94e407d4cef7fb8817ff59fe1d4020
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-27 11:08:20 +0800

    master spring spring整合JUnit

---
+++ spring-transaction/src/test/java/com/bylearning/spring/test/JdbcTemplateTest.java
@@ -0,0 +1,30 @@
+package com.bylearning.spring.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/9/27 10:50
+ * @description TODO
+ */
+// 指定当前测试类在Spring的测试环境中执行，此时就可以通过注入的方式直接获取IOC容器中的bean
+@RunWith(SpringJUnit4ClassRunner.class)
+// 设置Spring测试环境的配置文件
+@ContextConfiguration("classpath:spring-jdbc.xml")
+public class JdbcTemplateTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testInsert() {
+        String sql = "insert into t_user values(null, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, "root", "123", 23, "女", "123@qq.com");
+    }
+}
