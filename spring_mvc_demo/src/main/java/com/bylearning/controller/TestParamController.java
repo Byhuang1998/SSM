2f9e0e4500eee4a2d2ac48a1adba803916fc4535
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-28 11:03:56 +0800

    master SpringMVC 获取请求参数方法二及@RequestParam注解

--- spring_mvc_demo/src/main/java/com/bylearning/controller/TestParamController.java
+++ spring_mvc_demo/src/main/java/com/bylearning/controller/TestParamController.java
@@ -2,6 +2,7 @@ package com.bylearning.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,6 +14,10 @@ import javax.servlet.http.HttpServletRequest;
  * 获取请求参数的方式
  * 1、只需要在控制器方法的形参位置设置HttpServletRequest类型的形参，就可以在控制器方法
  * 使用request对象获取请求参数
+ * 2、通过控制器方法的形参获取
+ * 只需要在控制器形参的位置设置形参，但是要保持形参名称和传递过来的请求参数名相同
+ * 3、@RequestParam注解将请求参数和控制器方法的形参绑定
+ * 三个属性：value, required, defaultValue
  */
 @Controller
 public class TestParamController {
@@ -22,6 +27,14 @@ public class TestParamController {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println("username:" + username + ", password:" + password);
+        return "success";
+    }
+
+    @RequestMapping(value = "/param")
+    public String getParam(
+            @RequestParam(value = "username", required = false, defaultValue = "hello") String userName,
+            String password) {
+        System.out.println("username:" + userName + ", password:" + password);
         return "success";
     }
 }
