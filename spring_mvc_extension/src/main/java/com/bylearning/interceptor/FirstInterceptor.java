8aa5792507b016d6ae76bd21b3cf1e8757a3de3c
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-29 20:45:41 +0800

    master spring mvc 拦截器的执行顺序 源码分析

--- spring_mvc_extension/src/main/java/com/bylearning/interceptor/FirstInterceptor.java
+++ spring_mvc_extension/src/main/java/com/bylearning/interceptor/FirstInterceptor.java
@@ -12,7 +12,16 @@ import javax.servlet.http.HttpServletResponse;
  * @version 1.0
  * @data 2022/9/29 19:49
  * @description TODO
- */
+ * preHandle()：在控制器方法执行之前执行，其返回值表示对控制器方法的拦截或放行
+ * postHandle()：在控制器方法执行之后执行
+ * afterCompletion()：在控制器方法执行之后，且渲染视图完毕之后执行
+ *
+ * 多个拦截器执行顺序和在SpringMVC的配置文件中配置的顺序有关
+ * preHandle()按照配置的顺序执行，postHandle和afterCompletion按配置顺序反序执行
+ *
+ * 若拦截器中有某个拦截器的preHandle()返回了false：暂时略过
+ * 锻炼看源码的能力
+ **/
 @Component
 public class FirstInterceptor implements HandlerInterceptor {
 
