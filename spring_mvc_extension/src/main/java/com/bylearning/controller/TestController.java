11da0739e31359a900ec78e744679d553cc517e8
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-29 20:18:05 +0800

    master spring mvc 拦截器的配置

---
+++ spring_mvc_extension/src/main/java/com/bylearning/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.bylearning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/9/29 19:41
+ * @description TODO
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/test/hello")
+    public String testHello() {
+        return "success";
+    }
+}
---
+++ spring_mvc_extension/src/main/java/com/bylearning/interceptor/FirstInterceptor.java
@@ -0,0 +1,34 @@
+package com.bylearning.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/9/29 19:49
+ * @description TODO
+ */
+@Component
+public class FirstInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("FirstInterceptor --> preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("FirstInterceptor --> postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("FirstInterceptor --> afterCompletion");
+    }
+}
