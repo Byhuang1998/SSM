1ef9c55caac0cdaabb3379697634e40814169288
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-29 14:47:23 +0800

    master spring mvc处理ajax请求

--- spring_mvc_ajax/src/main/java/com/bylearning/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/bylearning/controller/TestAjaxController.java
@@ -1,6 +1,10 @@
 package com.bylearning.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author mskj-huangbingyi
@@ -10,4 +14,11 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class TestAjaxController {
+
+    @RequestMapping("/test/ajax")
+    public String testAjax(Integer id, HttpServletResponse response) throws IOException {
+        System.out.println("id:" + id);
+        response.getWriter().write("hello, axios");
+        return "success";
+    }
 }
