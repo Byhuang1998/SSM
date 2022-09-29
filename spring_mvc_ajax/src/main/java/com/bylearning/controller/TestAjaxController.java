92745115d60a6a2a76b8301660320fccb6e7d43e
mskj-huangbingyi <byhuang1998@gmail.com>
2022-09-29 12:48:21 +0800

    master spring mvc ajax准备工作

---
+++ spring_mvc_ajax/src/main/java/com/bylearning/controller/TestAjaxController.java
@@ -0,0 +1,13 @@
+package com.bylearning.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/9/29 11:10
+ * @description TODO
+ */
+@Controller
+public class TestAjaxController {
+}
