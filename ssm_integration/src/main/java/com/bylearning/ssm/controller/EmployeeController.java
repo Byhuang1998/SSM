d99b9a037cd5987e405d1aff408e136164619574
mskj-huangbingyi <byhuang1998@gmail.com>
2022-10-01 22:21:36 +0800

    master ssm 整合

---
+++ ssm_integration/src/main/java/com/bylearning/ssm/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.bylearning.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/10/1 21:47
+ * @description TODO
+ */
+@Controller
+public class EmployeeController {
+}
