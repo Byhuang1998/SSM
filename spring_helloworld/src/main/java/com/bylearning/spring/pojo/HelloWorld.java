de41ae9199f319878705c5bab5d7179cdebe8aa3
byhuang1998 <byhuang1998@gmail.com>
2024-01-22 23:21:01 +0800

    sprig xml

--- spring_helloworld/src/main/java/com/bylearning/spring/pojo/HelloWorld.java
+++ spring_helloworld/src/main/java/com/bylearning/spring/pojo/HelloWorld.java
@@ -8,15 +8,13 @@ package com.bylearning.spring.pojo;
  */
 public class HelloWorld {
 
-    public HelloWorld() {
-        // 覆盖无参构造方法，是为了验证bean的scope分别为singleton和prototype时，创建对象的时机
-        // 在Test代码中获取IoC容器那一行代码中打断点，执行测试方法
-        // 当scope=singleton时，IoC容器完成加载时，对象就创建了
-        // 当scope=prototype时，IoC容器完成加载时，还未创建对象，而是调用getBean方法时创建的
-        System.out.println("object is creating...");
-    }
+    private HelloSpring helloSpring;
 
     public void sayHello() {
-        System.out.println("hello, spring");
+        helloSpring.sayHello();
+    }
+
+    public void setHelloSpring(HelloSpring helloSpring) {
+        this.helloSpring = helloSpring;
     }
 }
